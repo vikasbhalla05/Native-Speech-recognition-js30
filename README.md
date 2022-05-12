f268587333cfc3441ad596754db409bcf4e10260
vikasbhalla05 <77003706+vikasbhalla05@users.noreply.github.com>
2022-05-12 08:03:32 +0530

    completed the speech recognition project

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Building the Intuition
 
 1. Get the `window.speechRecognition` and make a `new` instance of it.
-2. Put `recognition.interinResults` to populate results continuously as we speak.
+2. Put `recognition.interimResults` to populate results continuously as we speak.
 3. Create a 'p' element and append it to 'words' element as a child.
 4. Add an event listener to recognition for the event `result`
 5. Convert the coming event to array with `Array.from()` and map over it twice to get the transcript and then join them
--- speech.js
+++ speech.js
@@ -1,3 +1,28 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+let recognition = new SpeechRecognition();
 
 
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+let place = document.querySelector(".words");
+place.appendChild(p);
+
+recognition.addEventListener('result', e => {
+  const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('');
+
+      console.log(transcript);
+
+      p.textContent = transcript;
+
+      if(e.results[0].isFinal){
+        p = document.createElement('p');
+        place.appendChild(p);
+      }
+});
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
